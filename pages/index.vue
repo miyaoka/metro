e97b74739266907aa8a0df7176c7ac395f047717
Masaya Kazama <miyan@t-p.jp>
2018-03-02 15:51:15 +0900

    Add info view

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,56 @@
 <template>
   <div>
     <metro-map
-      width="800"
-      height="500"
+      :width="800"
+      :height="500"
       class="map"
     />
+
+    <div class="info">
+      <section>
+        <h2>stations</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="station in stations"
+              :key="station.idx"
+            >
+              <th>{{station.idx.substring(0, 3)}}</th>
+              <td>{{station.x.toFixed(0)}}</td>
+              <td>{{station.y.toFixed(0)}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section>
+        <h2>lines</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <td>station list</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="line in lines"
+              :key="line.idx"
+            >
+              <th>{{line.idx.substring(0, 3)}}</th>
+              <td>{{line.stations.map(st => st.substring(0, 3)).join(' - ')}}</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </section>
+    </div>
   </div>
 </template>
 
@@ -31,5 +77,12 @@ export default {
   background: #ffffee;
   margin: auto;
   display: block;
+}
+.info {
+  display: grid;
+  grid-template-columns: auto auto;
+  margin: auto;
+  justify-items: center;
+  width: 800px;
 }
 </style>
