d69f51ed7d4a99d6a8d430b71059e8f8c087d180
Masaya Kazama <miyan@t-p.jp>
2018-03-03 14:00:14 +0900

    Move nodelist function to store

--- components/MetroLine.vue
+++ components/MetroLine.vue
@@ -15,55 +15,16 @@
 <script>
 import { mapGetters } from 'vuex'
 
-class Point {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-  }
-  toString() {
-    return `${this.x},${this.y}`
-  }
-  add(point) {
-    return new Point(this.x + point.x, this.y + point.y)
-  }
-  subtract(point) {
-    return new Point(this.x - point.x, this.y - point.y)
-  }
-}
-
-const sign = (num) => (num === 0 ? 0 : num > 0 ? 1 : -1)
-const lineOffset = 5
-
 export default {
   props: {
     line: { type: Object },
     color: { type: String }
   },
   computed: {
-    ...mapGetters(['getStation']),
+    ...mapGetters(['getStation', 'getLineNodeList']),
 
     pathList() {
-      let stList = this.line.stations.map((id) => {
-        const st = this.getStation(id)
-        const offset = 0
-        //          st.lines.findIndex(val => val === this.line.id) * lineOffset
-        return new Point(st.x + offset, st.y + offset)
-      })
-
-      let pathNodeList = []
-
-      stList.slice(0, -1).forEach((currSt, i) => {
-        const nextSt = stList[i + 1]
-        const diff = nextSt.subtract(currSt)
-        const min = Math.min(Math.abs(diff.x), Math.abs(diff.y))
-        pathNodeList.push([
-          currSt,
-          currSt.add(new Point(min * sign(diff.x), min * sign(diff.y))),
-          nextSt
-        ])
-      })
-
-      return pathNodeList.map((nodes) => {
+      return this.getLineNodeList(this.line.id).map((nodes) => {
         const moveto = nodes.shift().toString()
         const lineto = nodes.reduce(
           (prev, curr) => `${prev} ${curr.toString()}`
--- store/index.js
+++ store/index.js
@@ -1,3 +1,22 @@
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+  toString() {
+    return `${this.x},${this.y}`
+  }
+  add(point) {
+    return new Point(this.x + point.x, this.y + point.y)
+  }
+  subtract(point) {
+    return new Point(this.x - point.x, this.y - point.y)
+  }
+}
+
+const sign = (num) => (num === 0 ? 0 : num > 0 ? 1 : -1)
+const lineOffset = 5
+
 export const state = () => ({
   stations: {},
   lines: {}
@@ -5,7 +24,29 @@ export const state = () => ({
 
 export const getters = {
   getStation: (state) => (id) => state.stations[id],
-  getLine: (state) => (id) => state.lines[id]
+  getLine: (state) => (id) => state.lines[id],
+  getLineNodeList: (state, getters) => (lineId) => {
+    let stList = getters.getLine(lineId).stations.map((stId) => {
+      const st = getters.getStation(stId)
+      const offset = 0
+      //          st.lines.findIndex(val => val === this.line.id) * lineOffset
+      return new Point(st.x + offset, st.y + offset)
+    })
+
+    let nodeList = []
+
+    stList.slice(0, -1).forEach((currSt, i) => {
+      const nextSt = stList[i + 1]
+      const diff = nextSt.subtract(currSt)
+      const min = Math.min(Math.abs(diff.x), Math.abs(diff.y))
+      nodeList.push([
+        currSt,
+        currSt.add(new Point(min * sign(diff.x), min * sign(diff.y))),
+        nextSt
+      ])
+    })
+    return nodeList
+  }
 }
 
 export const mutations = {
